5b7657f3c5ba7ddce7da72695d1800a66ffcfac4
Ruotger Deecke <roddi@me.com>
2015-12-26 11:59:06 +0100

    started engine refactor

---
+++ DataSource/DataSourceEngine.swift
@@ -0,0 +1,9 @@
+//
+//  DataSourceEngine.swift
+//  FURRDataSource
+//
+//  Created by Ruotger Deecke on 26.12.15.
+//  Copyright © 2015 Ruotger Deecke. All rights reserved.
+//
+
+import Foundation
